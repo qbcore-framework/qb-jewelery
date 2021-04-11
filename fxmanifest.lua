e2230ff30f436df813c09a899563d04524e2e048
GhzGarage <qbcore@programmer.net>
2021-04-10 21:54:16 -0500

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Jewelry'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua',
+    'config.lua',
+}
+
+server_scripts {
+    'server/main.lua',
+    'config.lua',
+}
