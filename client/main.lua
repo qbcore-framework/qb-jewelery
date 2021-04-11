3c679e66830849ce87e448c32a9ab546fb97e33f
GhzGarage <qbcore@programmer.net>
2021-04-10 22:47:58 -0500

    Keys Stuff

--- client/main.lua
+++ client/main.lua
@@ -60,7 +60,7 @@ Citizen.CreateThread(function()
                             if dist < 0.6 then
                                 if not Config.Locations[case]["isBusy"] and not Config.Locations[case]["isOpened"] then
                                     DrawText3Ds(Config.Locations[case]["coords"]["x"], Config.Locations[case]["coords"]["y"], Config.Locations[case]["coords"]["z"], '[E] Storing the display case')
-                                    if IsControlJustPressed(0, Keys["E"]) then
+                                    if IsControlJustPressed(0, 38) then
                                         QBCore.Functions.TriggerCallback('qb-jewellery:server:getCops', function(cops)
                                             if cops >= Config.RequiredCops then
                                                 if validWeapon() then
