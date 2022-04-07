fb457fe41cb6931dc5e4dee1c108a36b468494b8
El Vod Kaa <44878760+elvodkaa@users.noreply.github.com>
2022-04-07 23:43:49 +0100

    Create ar.lua

---
+++ locale/ar.lua
@@ -0,0 +1,19 @@
+local Translations = {
+    error = {
+        fingerprints = 'لقد ترك بصمة على الزجاج',
+        minimum_police = 'شرطي %{value} يلزم توجد',
+        wrong_weapon = 'سلاحك ليس قويا بما فيه الكفاية',
+        to_much = 'لديك الكثير في حقيبتك'
+    },
+    success = {},
+    info = {
+        progressbar = 'جاري الكسر',
+    },
+    general = {
+        target_label = 'تحطيم و كسر',
+        drawtextui_grab = '[E] تحطيم الشباك',
+        drawtextui_broken = 'الحالة: مكسورة'
+    }
+}
+
+Lang = Locale:new({phrases = Translations})
