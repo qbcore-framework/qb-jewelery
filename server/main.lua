cf9ac48255991e85540c46601d30aa45c7144781
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-08-30 20:55:20 +0200

    Fix exploitable event

--- server/main.lua
+++ server/main.lua
@@ -1,50 +1,79 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local timeOut = false
 
+local cachedPoliceAmount = {}
+
 -- Callback
 
-QBCore.Functions.CreateCallback('qb-jewellery:server:getCops', function(_, cb)
+QBCore.Functions.CreateCallback('qb-jewellery:server:getCops', function(source, cb)
 	local amount = 0
     for _, v in pairs(QBCore.Functions.GetQBPlayers()) do
         if v.PlayerData.job.name == "police" and v.PlayerData.job.onduty then
             amount = amount + 1
         end
     end
+    cachedPoliceAmount[source] = amount
     cb(amount)
 end)
 
 -- Events
 
 RegisterNetEvent('qb-jewellery:server:setVitrineState', function(stateType, state, k)
-    Config.Locations[k][stateType] = state
-    TriggerClientEvent('qb-jewellery:client:setVitrineState', -1, stateType, state, k)
+    if stateType == "isBusy" and type(state) == "boolean" and Config.Locations[k] then
+        Config.Locations[k][stateType] = state
+        TriggerClientEvent('qb-jewellery:client:setVitrineState', -1, stateType, state, k)
+    end
 end)
 
 QBCore.Functions.CreateCallback('qb-jewellery:server:getVitrineState', function(_, cb)
 	cb(Config.Locations)
 end)
 
-RegisterNetEvent('qb-jewellery:server:vitrineReward', function()
+RegisterNetEvent('qb-jewellery:server:vitrineReward', function(vitrineIndex)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local otherchance = math.random(1, 4)
     local odd = math.random(1, 4)
 
-    if otherchance == odd then
-        local item = math.random(1, #Config.VitrineRewards)
-        local amount = math.random(Config.VitrineRewards[item]["amount"]["min"], Config.VitrineRewards[item]["amount"]["max"])
-        if Player.Functions.AddItem(Config.VitrineRewards[item]["item"], amount) then
-            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.VitrineRewards[item]["item"]], 'add')
-        else
-            TriggerClientEvent('QBCore:Notify', src, Lang:t('error.to_much'), 'error')
+    if Config.Locations[vitrineIndex] == nil or Config.Locations[vitrineIndex].isOpened ~= false or cachedPoliceAmount[source] == nil then 
+        DropPlayer(src, "Exploiting") 
+    end
+
+    local plrPed = GetPlayerPed(src)
+    local plrCoords = GetEntityCoords(plrPed)
+    local vitrineCoords = Config.Locations[vitrineIndex].coords
+
+    if cachedPoliceAmount[source] >= Config.RequiredCops then
+        if plrPed then
+            local dist = #(plrCoords - vitrineCoords)
+            if dist <= 25.0 then
+                Config.Locations[vitrineIndex]["isOpened"] = true
+                Config.Locations[vitrineIndex]["isBusy"] = false
+                TriggerClientEvent('qb-jewellery:client:setVitrineState', -1, "isOpened", true, vitrineIndex)
+                TriggerClientEvent('qb-jewellery:client:setVitrineState', -1, "isBusy", false, vitrineIndex)
+
+                if otherchance == odd then
+                    local item = math.random(1, #Config.VitrineRewards)
+                    local amount = math.random(Config.VitrineRewards[item]["amount"]["min"], Config.VitrineRewards[item]["amount"]["max"])
+                    if Player.Functions.AddItem(Config.VitrineRewards[item]["item"], amount) then
+                        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.VitrineRewards[item]["item"]], 'add')
+                    else
+                        TriggerClientEvent('QBCore:Notify', src, Lang:t('error.to_much'), 'error')
+                    end
+                else
+                    local amount = math.random(2, 4)
+                    if Player.Functions.AddItem("10kgoldchain", amount) then
+                        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["10kgoldchain"], 'add')
+                    else
+                        TriggerClientEvent('QBCore:Notify', src, Lang:t('error.to_much'), 'error')
+                    end
+                end
+            else
+                DropPlayer(src, "Exploiting") 
+            end
         end
     else
-        local amount = math.random(2, 4)
-        if Player.Functions.AddItem("10kgoldchain", amount) then
-            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["10kgoldchain"], 'add')
-        else
-            TriggerClientEvent('QBCore:Notify', src, Lang:t('error.to_much'), 'error')
-        end
+        DropPlayer(src, "Exploiting") 
     end
 end)
 
