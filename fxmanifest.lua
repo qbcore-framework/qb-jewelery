de44f015e31197e168f8cf2be802920342cf637b
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2024-02-11 05:58:09 -0600

    Description for txAdmin

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
-
-description 'QB-Jewelry'
+lua54 'yes'
+author 'Kakarot'
+description 'Allows players to rob jewelry stores for items to sell'
 version '1.2.0'
 
 shared_scripts {
@@ -20,5 +21,3 @@ server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/main.lua'
 }
-
-lua54 'yes'
